7ba9ffb93d5bde2860409f17d05f982571d94f1c
Sam Chatfield <s.chatfield@kainos.com>
2023-02-04 13:06:35 +0000

    Convert to GOV.UK template.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
 import React from "react";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Template } from "./components/govuk";
 
-function App() {
-  const [count, setCount] = useState(0);
+const heading = "GOV.UK Vite React Starter";
 
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Template serviceName={heading}>
+      <h1 className="govuk-heading-l">{heading}</h1>
+      <p className="govuk-body">Hello, World!</p>
+    </Template>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
